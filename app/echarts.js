5c8a885f63b67c592c497e58a597a81d0c32a94e
ruiwcdl <ruiwcdl@cn.ibm.com>
2018-01-22 08:37:28 +0800

    add baidu's echarts

--- app/echarts.js
+++ app/echarts.js
@@ -4,7 +4,7 @@
 
 import React, {Component} from 'react'
 import styles from './css/echarts.css';
-import echarts from 'echarts';
+import echarts from '../echarts';
 class Echarts extends Component{
 	
 	
